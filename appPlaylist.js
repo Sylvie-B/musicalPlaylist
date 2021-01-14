49f89d0d5119a7eec8da633699f8326468587808
Sylvie-B <unsolo@hotmail.fr>
2021-01-14 10:08:42 +0100

    html

---
+++ appPlaylist.js
@@ -0,0 +1,19 @@
+
+// tableau : list <-- playlist = objet <-- tableau : titles <-- objet = morceau
+// responsive
+
+let list = [
+
+]
+
+let Playlist = function (listName, listGenre){
+    this.name = listName;
+    this.genre = listGenre;
+
+    let titles = [
+
+    ];
+
+
+}
+
